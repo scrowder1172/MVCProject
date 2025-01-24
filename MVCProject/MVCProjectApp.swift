2429420c43091f3492ed3af88c6a3be99b136834
Scott Crowder <scrowder@gmail.com>
2025-01-24 09:34:49 -0600

    Initial Commit

---
+++ MVCProject/MVCProjectApp.swift
@@ -0,0 +1,20 @@
+//
+// File: MVCProjectApp.swift
+// Project: MVCProject
+// 
+// Created by SCOTT CROWDER on 1/24/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct MVCProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
